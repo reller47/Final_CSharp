e3bfdc978fe1bda4a3f6e2f60984894d55ee0d44
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-09-17 15:17:18 +0300

    Task_66

---
+++ Task_66/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter M = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for(int i = m; i <= n; i++){
+    sum = sum + i;
+}
+Console.Write("Result  = " + sum);
