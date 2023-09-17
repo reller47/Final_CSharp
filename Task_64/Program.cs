2178c943a60b616a98827e57d12c318196231ea9
Kirill Mikhaylov <tytkaktyt463@gmail.com>
2023-09-17 15:04:30 +0300

    Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+//int i = 0;
+method(n);
+int method(int n){
+    Console.Write(n + " ");
+    if(n == 1){
+        Console.WriteLine("");
+        Console.WriteLine("End");
+        return 0;
+    }else{
+        return method(n - 1);
+    }
+}
